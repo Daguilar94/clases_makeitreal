1f11a9063f23e7a49d041751c8a31617083bc665
dguilar1994 <andrea@MacBook-Pro-de-Andrea.local>
2017-05-23 22:03:49 -0500

    Incluir los archivos y las carpetas de gemas y sinatra (en las clases 10 y 11 no tengo nada)

---
+++ clase12/sinatra_app/app.rb
@@ -0,0 +1,61 @@
+require 'sinatra'
+
+get '/' do
+  puts "PARAMS = #{params}"
+  @nombre = params[:name]
+  erb :index
+end
+
+get '/holamundo' do
+  puts params
+  'hola mundo'
+end
+
+get '/orders' do
+  #puts "#{params[:search]}" #Asi se puede coger el contedido de los formularios web
+  @orders = [
+    {order_id: 'o_ifjnlifb', price: 5000},
+    {order_id: 'o_ifjnrfds', price: 5000},
+    {order_id: 'o_ifjnlfgh', price: 5000},
+    {order_id: 'o_ifqwesxx', price: 5000},
+  ]
+
+  @order = @orders.find { |order|
+  order[:order_id] == params[:order_id]
+  }
+  erb :orders
+end
+
+get '/orders/:order_id' do
+  puts "ORDER ID=#{params[:order_id]}"
+  puts "SORT BY=#{params[:sort]}"
+end
+
+get '/customers/:email/orders' do
+  puts "ORDER ID=#{params[:email]}"
+end
+
+get '/login' do
+  erb :login
+end
+
+post '/login' do
+  email = params[:email]
+  password = params[:password]
+  puts "EMAIL = #{params[:email]}"
+  puts "PASSWORD = #{params[:password]}"
+  if email.empty? || password.empty?
+    @errors = [{message: "Email and password can't be empty"}]
+    return error 400, erb(:login)
+  end
+  unless email == 'david@gmail.com' && password == '1234'
+    @errors = [{message: "Email and password doesn't match"}]
+    return error 400, erb(:login)
+  end
+    redirect "/user_account/#{params[:email]}"
+end
+
+get '/user_account/:email' do
+  @email = params[:email]
+  erb :account
+end
